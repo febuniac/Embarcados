40909a7ba6d153f59b5cbce7280bfb4f979b49f7
Buniac <febuniac@gmail.com>
2017-12-08 16:44:15 -0200

    TC e RTC done

--- Códigos/Códigos Feitos/09 - TC e RTC/src/src/main.c
+++ Códigos/Códigos Feitos/09 - TC e RTC/src/src/main.c
@@ -10,10 +10,10 @@
  *  ou ser atualizado pelo PC.
  */
 #define YEAR        2017
-#define MOUNTH      9
-#define DAY         20
+#define MOUNTH      12
+#define DAY         10
 #define WEEK        13
-#define HOUR        9
+#define HOUR        4
 #define MINUTE      5
 #define SECOND      0
 
@@ -100,7 +100,22 @@ void LED_init(int estado);
 void TC1_init(void);
 void RTC_init(void);
 void pin_toggle(Pio *pio, uint32_t mask);
+//declarando as funcoes dos  botoes e leds criados
+void but_init(void);
+void but1_init(void);
+void but2_init(void);
+void but3_init(void);
 
+void but_Handler();
+void but_Handler1();
+void but_Handler2();
+void but_Handler3();
+void TC0_init(void);
+void TC1_init(void);
+void TC2_init(void);
+void TC3_init(void);
+void RTC_init(void);
+void pin_toggle(Pio *pio, uint32_t mask);
 /************************************************************************/
 /* Handlers                                                             */
 /************************************************************************/
@@ -201,7 +216,8 @@ void TC0_Handler(void){
 	UNUSED(ul_dummy);
 
 	/** Muda o estado do LED */
-	pin_toggle(LED_PIO, LED_PIN_MASK);
+	if(flag_led0)
+		pin_toggle(LED_PIO, LED_PIN_MASK);
 
 }
 
@@ -223,7 +239,7 @@ void TC1_Handler(void){
 	UNUSED(ul_dummy);
 	/** Muda o estado do LED */
     if(flag_led0)
-        pin_toggle(LED_PIO, LED_PIN_MASK);
+        pin_toggle(LED1_PIO, LED1_PIN_MASK);
 		
 }
 
@@ -247,8 +263,8 @@ void TC2_Handler(void){
 
 	UNUSED(ul_dummy);
 	/** Muda o estado do LED */
-
-	pin_toggle(LED2_PIO, LED2_PIN_MASK);
+	if(flag_led0)
+		pin_toggle(LED2_PIO, LED2_PIN_MASK);
 
 }
 
@@ -270,7 +286,8 @@ void TC3_Handler(void){
 	/* Avoid compiler warning */
 	UNUSED(ul_dummy);
 	/** Muda o estado do LED */
-	pin_toggle(LED3_PIO, LED3_PIN_MASK);
+	if(flag_led0)
+		pin_toggle(LED3_PIO, LED3_PIN_MASK);
 }
 
 
@@ -289,18 +306,151 @@ void RTC_Handler(void)
 
 	/* Second increment interrupt */
 	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
+		
+		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+
+		} else {
+		/* Time or date alarm */
+		if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
+			
+			/*Atualiza hora */
+			uint32_t ano,mes,dia,hora,minuto,segundo;
+			
+			rtc_get_date(RTC, &ano, &mes, &dia, NULL);
+			rtc_get_time(RTC, &hora, &minuto, &segundo);
+			
+			/* incrementa minuto (Para casos de mudança de hora ou de mes ou de ano) */
+			if(minuto>=59){
+				if(hora>=23){
+					if(dia>=30){
+						if(mes>=11){
+							ano++;
+						}
+						else{
+							mes++;
+						}
+						
+					}
+					else{
+						dia++;
+					}
+				}
+				else{
+					hora++;
+				}
+			}
+			else{
+				minuto++;
+			}
+			
+			
+			/* configura novo alarme do RTC */
+			rtc_set_date_alarm(RTC, 1, mes, 1, dia);
+			rtc_set_time_alarm(RTC, 1, hora, 1, minuto, 1, segundo);
+			
+			/* inverte status led */
+			flag_led0 ^= 1;
+			
+			/* Ativa/desativa o TimerCounter */
+			if(flag_led0){
+				tc_start(TC0, 0);
+				tc_start(TC0, 1);
+				tc_start(TC0, 2);
+				tc_start(TC1, 0);
+			}
+			else{
+				tc_stop(TC0, 0);
+				tc_stop(TC0, 1);
+				tc_stop(TC0, 2);
+				tc_stop(TC1, 0);
+			}
+			
+			rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+		}
+	}
+/*	
+	uint32_t ul_status = rtc_get_status(RTC);
+
+	/* Second increment interrupt 
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
 	
 		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
 
 	} else {
-		/* Time or date alarm */
+		/* Time or date alarm 
 		if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
       flag_led0 = 0;    
        
 			rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
 		}
-	}
+	}*/
 }
+//Atuação do botão 1 no led 1
+void but_Handler1(){
+    /*
+     *  limpa interrupcao do PIO
+     */
+    uint32_t pioIntStatus;
+    pioIntStatus =  pio_get_interrupt_status(BUT1_PIO);
+    
+   /**
+    *  Toggle status led
+    */
+   if(pio_get_output_data_status(LED1_PIO, LED1_PIN_MASK)){
+    pio_clear(LED1_PIO, LED1_PIN_MASK);
+	tc_start(TC0,1);
+   }
+   else{
+    pio_set(LED1_PIO,LED1_PIN_MASK);
+	tc_stop(TC0,1);
+   }
+    
+}
+
+//Atuação do botão 2 no led 2
+void but_Handler2(){
+    /*
+     *  limpa interrupcao do PIO
+     */
+    uint32_t pioIntStatus;
+    pioIntStatus =  pio_get_interrupt_status(BUT2_PIO);
+    
+   /**
+    *  Toggle status led
+    */
+   if(pio_get_output_data_status(LED2_PIO, LED2_PIN_MASK)){
+    pio_clear(LED2_PIO, LED2_PIN_MASK);
+	tc_start(TC0,2);
+   }
+   else{
+    pio_set(LED2_PIO,LED2_PIN_MASK);
+    tc_stop(TC0,2);
+   
+}
+}
+
+//Atuação do botão 3 no led 3
+void but_Handler3(){
+    /*
+     *  limpa interrupcao do PIO
+     */
+    uint32_t pioIntStatus;
+    pioIntStatus =  pio_get_interrupt_status(BUT3_PIO);
+    
+   /**
+    *  Toggle status led
+    */
+   if(pio_get_output_data_status(LED3_PIO, LED3_PIN_MASK)){
+    pio_clear(LED3_PIO, LED3_PIN_MASK);
+	tc_start(TC1,0);
+   }
+   else{
+    pio_set(LED3_PIO,LED3_PIN_MASK);
+	tc_stop(TC1,0);
+   }
+    
+}
+
 
 
 /************************************************************************/
@@ -328,7 +478,7 @@ void BUT_init(void){
     /* config. interrupcao em borda de descida no botao do kit */
     /* indica funcao (but_Handler) a ser chamada quando houver uma interrupção */
     pio_enable_interrupt(BUT_PIO, BUT_PIN_MASK);
-    pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIN_MASK, PIO_IT_FALL_EDGE, Button1_Handler);
+    pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIN_MASK, PIO_IT_FALL_EDGE, but1_Handler);
     
     /* habilita interrupçcão do PIO que controla o botao */
     /* e configura sua prioridade                        */
@@ -340,14 +490,48 @@ void BUT_init(void){
  * @Brief Inicializa o pino do LED
  */
 void LED_init(int estado){
-    pmc_enable_periph_clk(LED_PIO_ID);
-    pio_set_output(LED_PIO, LED_PIN_MASK, estado, 0, 0 );
+
+	//inicializando os 3 LEDs
+	//LED1 inicializado
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pio_set_output(LED1_PIO, LED1_PIN_MASK, estado, 0, 0 );
+	
+	//LED2 inicializado
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pio_set_output(LED2_PIO, LED2_PIN_MASK, estado, 0, 0 );
+	
+	//LED3 inicializado
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	pio_set_output(LED3_PIO, LED3_PIN_MASK, estado, 0, 0 );
+
 };
 
 /**
  * Configura TimerCounter (TC0) para gerar uma interrupcao no canal 0-(ID_TC1) 
  * a cada 250 ms (4Hz)
  */
+void TC0_init(void){
+	uint32_t ul_div;
+	uint32_t ul_tcclks;
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+	
+	uint32_t channel = 0;
+	
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_TC0);
+
+	/** Configura o TC para operar em  4Mhz e interrupçcão no RC compare */
+	tc_find_mck_divisor(4, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
+	tc_init(TC0, channel, ul_tcclks | TC_CMR_CPCTRG);
+	tc_write_rc(TC0, channel, (ul_sysclk / ul_div) / 4);
+
+	/* Configura e ativa interrupçcão no TC canal 0 */
+	NVIC_EnableIRQ((IRQn_Type) ID_TC0);
+	tc_enable_interrupt(TC0, channel, TC_IER_CPCS);
+
+	/* Inicializa o canal 0 do TC */
+	tc_start(TC0, channel);
+}
 void TC1_init(void){   
     uint32_t ul_div;
     uint32_t ul_tcclks;
@@ -372,7 +556,53 @@ void TC1_init(void){
     /* Inicializa o canal 0 do TC */
     tc_start(TC0, channel);
 }
+void TC2_init(void){
+	uint32_t ul_div;
+	uint32_t ul_tcclks;
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+	
+	uint32_t channel = 2;
+	
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_TC2);
 
+	/** Configura o TC para operar em  4Mhz e interrupçcão no RC compare */
+	tc_find_mck_divisor(11, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
+	tc_init(TC0, channel, ul_tcclks | TC_CMR_CPCTRG);
+	tc_write_rc(TC0, channel, (ul_sysclk / ul_div) / 11);
+
+	/* Configura e ativa interrupçcão no TC canal 0 */
+	NVIC_EnableIRQ((IRQn_Type) ID_TC2);
+	tc_enable_interrupt(TC0, channel, TC_IER_CPCS);
+
+	/* Inicializa o canal 0 do TC */
+	tc_start(TC0, channel);
+}
+
+
+
+void TC3_init(void){
+	uint32_t ul_div;
+	uint32_t ul_tcclks;
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+	
+	uint32_t channel = 0;
+	
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_TC3);
+
+	/** Configura o TC para operar em  4Mhz e interrupçcão no RC compare */
+	tc_find_mck_divisor(17, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
+	tc_init(TC1, channel, ul_tcclks | TC_CMR_CPCTRG);
+	tc_write_rc(TC1, channel, (ul_sysclk / ul_div) / 17);
+
+	/* Configura e ativa interrupçcão no TC canal 0 */
+	NVIC_EnableIRQ((IRQn_Type) ID_TC3);
+	tc_enable_interrupt(TC1, channel, TC_IER_CPCS);
+
+	/* Inicializa o canal 0 do TC */
+	tc_start(TC1, channel);
+}
 /**
  * Configura o RTC para funcionar com interrupcao de alarme
  */
@@ -398,6 +628,55 @@ void RTC_init(){
     rtc_enable_interrupt(RTC,  RTC_IER_ALREN); 
     
 }
+//Inicializando os três botões
+//BOTAO1 inicializado
+void but1_init(void){
+	/* config. pino botao em modo de entrada */
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pio_set_input(BUT1_PIO, BUT1_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	
+	/* config. interrupcao em borda de descida no botao do kit */
+	/* indica funcao (but_Handler) a ser chamada quando houver uma interrupção */
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIN_MASK);
+	pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_PIN_MASK, PIO_IT_FALL_EDGE, but_Handler1); ;//aciona na borda de descida   PIO_IT_FALL_EDGE
+	/* habilita interrupçcão do PIO que controla o botao */
+	/* e configura sua prioridade                        */
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 1);
+};
+
+//BOTAO2 inicializado
+void but2_init(void){
+	/* config. pino botao em modo de entrada */
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pio_set_input(BUT2_PIO, BUT2_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	
+	/* config. interrupcao em borda de descida no botao do kit */
+	/* indica funcao (but_Handler) a ser chamada quando houver uma interrupção */
+	pio_enable_interrupt(BUT2_PIO, BUT2_PIN_MASK);
+	pio_handler_set(BUT2_PIO, BUT2_PIO_ID, BUT2_PIN_MASK, PIO_IT_RISE_EDGE, but_Handler2);;//aciona na borda de subida &PIO_IT_RISE_EDGE
+	/* habilita interrupçcão do PIO que controla o botao */
+	/* e configura sua prioridade                        */
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_SetPriority(BUT2_PIO_ID, 1);
+};
+
+//BOTAO3 inicializado
+void but3_init(void){
+	/* config. pino botao em modo de entrada */
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	pio_set_input(BUT3_PIO, BUT3_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	
+	/* config. interrupcao em borda de descida no botao do kit */
+	/* indica funcao (but_Handler) a ser chamada quando houver uma interrupção */
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIN_MASK);
+	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIN_MASK, PIO_IT_RISE_EDGE, but_Handler3);//aciona na borda de subida PIO_IT_RISE_EDGE
+	
+	/* habilita interrupçcão do PIO que controla o botao */
+	/* e configura sua prioridade                        */
+	NVIC_EnableIRQ(BUT3_PIO_ID);
+	NVIC_SetPriority(BUT3_PIO_ID, 1);
+};
 
 /************************************************************************/
 /* Main Code	                                                        */
@@ -413,10 +692,17 @@ int main(void){
   LED_init(0);
 	
 	/* Configura os botões */
-	BUT_init();    
+	BUT_init();
+	but1_init();
+	but2_init();
+	but3_init();    
     
   /** Configura timer 0 */
+  TC0_init();
   TC1_init();//faz o led piscar em detrminada frequência
+  TC2_init();
+  TC3_init();
+  
     
   /** Configura RTC */
   RTC_init();
@@ -426,6 +712,7 @@ int main(void){
   rtc_set_time_alarm(RTC, 1, HOUR, 1, MINUTE, 1, SECOND+5);
   
   	while (1) {
+		  /* Entra em modo sleep */
     
 	}
 

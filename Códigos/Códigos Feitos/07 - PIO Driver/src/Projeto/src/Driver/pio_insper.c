4621e31536a7ef1f9a811202b1c70c23bdaed42e
Buniac <febuniac@gmail.com>
2017-12-08 16:16:57 -0200

    Driver

--- Códigos/Códigos Feitos/07 - PIO Driver/src/Projeto/src/Driver/pio_insper.c
+++ Códigos/Códigos Feitos/07 - PIO Driver/src/Projeto/src/Driver/pio_insper.c
@@ -28,10 +28,14 @@ ul_mask	Bitmask of one or more pin(s) to configure.
 ul_pull_up_enable	Indicates if the pin(s) internal pull-up shall be configured. 
 */
 void _pio_pull_up(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_pull_up_enable) {
-	if (ul_pull_up_enable)
-	p_pio->PIO_PUER = ul_mask; // Pull Up Enable Register
-	else
-	p_pio->PIO_PUDR = ul_mask; // Pull Up Disable Register
+	if (ul_pull_up_enable){
+		p_pio->PIO_PUER = ul_mask; // Pull Up Enable Register
+	}
+	
+	else{
+		p_pio->PIO_PUDR = ul_mask; // Pull Up Disable Register
+		
+	}
 }
 
 /*Configure PIO pin internal pull-down.
@@ -56,8 +60,25 @@ ul_default_level	Default level on the pin(s).
 ul_multidrive_enable	Indicates if the pin(s) shall be configured as open-drain.
 ul_pull_up_enable	Indicates if the pin shall have its pull-up activated.*/
 void pio_set_output	(Pio * 	p_pio,const uint32_t ul_mask,const uint32_t ul_default_level,const uint32_t ul_multidrive_enable,const uint32_t ul_pull_up_enable){
+	p_pio->PIO_PER = ul_mask; //  PIO Enable Register (Ativa controle do pino no PIO)
+	p_pio->PIO_OER = ul_mask;//	 Output Enable register (Ativa saída)
 	
+	if(!ul_pull_up_enable)// Checa se pullup nao esta enabled
+	{
+		p_pio->PIO_PUER	 = ul_mask;// Pull Up Enable Register
+	}
+	if(!ul_default_level)// Checa pela inicialização desejada
+	{
+		p_pio->PIO_CODR = ul_mask;//Clear Output Data Register (0 --> saída)
+	}
+	
+	else
+	{
+	p_pio->PIO_SODR = ul_mask;//Set Output Data Register (1 --> saída)
+	}
 }
+	
+
 
 /*Configure one or more pin(s) or a PIO controller as inputs.
 Optionally, the corresponding internal pull-up(s) and glitch filter(s) can be enabled.
@@ -67,7 +88,12 @@ ul_mask	Bitmask indicating which pin(s) to configure as input(s).
 ul_attribute	PIO attribute(s).*/
 
 void pio_set_input	(Pio * 	p_pio,const uint32_t ul_mask,const uint32_t ul_attribute){
-	
+	p_pio->PIO_ODR	 = ul_mask;//Output Disable Register
+	p_pio->PIO_PER	 = ul_mask;////Output Enable Register
+	_pio_pull_up(p_pio,ul_mask,PIO_PULLUP & ul_attribute);
+	if(PIO_DEBOUNCE & ul_attribute){
+		p_pio->PIO_IFER	 = ul_mask; // Debouncing ativado
+	}
 }
 
 /*Return 1 if one or more PIOs of the given Pin are configured to output a high level (even if they are not output).
@@ -77,10 +103,9 @@ uint32_t pio_get_output_data_status	(const Pio * p_pio, const uint32_t 	ul_mask)
 	if ((p_pio->PIO_ODSR & ul_mask) == 0)
 		return 0;
 	else
-		return 1
+		return 1;
 }
 
 
 //pull-up,pio_set_output, pio_set_input,_pio_get_output_data_status
-http://asf.atmel.com/docs/latest/common.components.wifi.winc1500.ap_scan_example.sam4s_xplained_pro/html/group__sam__drivers__pio__group.html#ga462f21899dd66756a34de15beb15474d
 
